9b8b27c18aa5717c30460fb3b229e161b5adbc9a
metin.irden <metin.irden@trendyol.com>
2024-05-19 13:08:57 +0300

    basic attribute source generator added

---
+++ src/Refithance.Generator/SourceGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Refithance.Generator;
+
+[Generator]
+internal sealed class SourceGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(
+            ctx => ctx.AddSource(SourceGeneratorHelpers.AttributeFileName,
+                SourceText.From(SourceGeneratorHelpers.Attribute, Encoding.UTF8)
+            )
+        );
+    }
+}
--- src/Refithance/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Refithance
-{
-    public class Class1
-    {
-
-    }
-}
